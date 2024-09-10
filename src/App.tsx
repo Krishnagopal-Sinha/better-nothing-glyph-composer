b8c5d5364a87ef1d045d53c7ecc96bf9eb09ace1
Krishnagopal-Sinha <ayushsinha24@gmail.com>
2024-09-10 12:19:52 +0530

    fix: added back exit confimation

--- src/App.tsx
+++ src/App.tsx
@@ -22,17 +22,17 @@ import { kWidthBound } from './lib/consts';
 export default function App() {
   // Promot user for exit confimation - leave it upto browser
 
-  // useEffect(() => {
-  //   function beforeUnload(e: BeforeUnloadEvent) {
-  //     e.preventDefault();
-  //     return '';
-  //   }
+  useEffect(() => {
+    function beforeUnload(e: BeforeUnloadEvent) {
+      e.preventDefault();
+      return '';
+    }
 
-  //   window.addEventListener('beforeunload', beforeUnload);
-  //   return () => {
-  //     window.removeEventListener('beforeunload', beforeUnload);
-  //   };
-  // }, []);
+    window.addEventListener('beforeunload', beforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', beforeUnload);
+    };
+  }, []);
 
   // App state
   const timelineData = useGlobalAppStore((state) => state.items);
